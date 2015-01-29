ce195ab49e1b3a8215a20996eb1bacfd7dc5037c
lvzhang <lvzhang@actiontec.com>
2015-01-29 23:06:43 +0800

    Exception fixed

--- src/android/src/TencentShare.java
+++ src/android/src/TencentShare.java
@@ -1,41 +1,16 @@
 package com.share.tencentShare;
 
-import java.io.FileNotFoundException;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.FileOutputStream;
-import java.io.File;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.FileHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.util.Log;
-import android.content.Context;
-import android.provider.MediaStore.Images.Media;
-import android.net.Uri;
-import android.os.Environment;
-import android.util.Base64;
-
-import java.util.ArrayList;
-
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.tencent.connect.share.QQShare;
 import com.tencent.connect.share.QzoneShare;
@@ -58,33 +33,37 @@ public class TencentShare extends CordovaPlugin {
       this.callbackContext = callbackContext;
       JSONObject jsonObject = args.getJSONObject(0);
       String appid = jsonObject.getString("appid");
-      if(mTencent = null){
-          mTencent = Tencent.createInstance(appid, this.getApplicationContext());
+      if (mTencent == null) {
+        mTencent = Tencent.createInstance(appid, this.cordova.getActivity());
       }
       title = jsonObject.getString("title");
       summary = jsonObject.getString("summary");
       target_url = jsonObject.getString("target_url");
       image_url = jsonObject.getString("image_url");
-      
+
+      JSONObject result = new JSONObject();
+      result.put("result", true);
+
       if (action.equals("qqShare")) {
-        sharetoQQ(title,summary,target_url,image_url);
+        sharetoQQ(title, summary, target_url, image_url);
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
         return true;
-      } else if(action.equals("qzoneShare")){
-        sharetoQzone(title,summary,target_url,image_url);
+      } else if (action.equals("qzoneShare")) {
+        sharetoQzone(title, summary, target_url, image_url);
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
         return true;
-      }else {
+      } else {
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
         return false;
       }
-    } catch(JSONException e) {
-    	Log.e("Protonet", "JSON Exception during the Image Base64 Plugin... :(");
+    } catch (JSONException e) {
+      Log.e("Protonet", "JSON Exception Plugin... :(");
     }
     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
     return false;
   }
-public void sharetoQQ(String title,String summary,String target_url,String image_url) {
+
+  public void sharetoQQ(String title, String summary, String target_url, String image_url) {
     Bundle bundle = new Bundle();
     bundle.clear();
     bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
@@ -93,10 +72,10 @@ public void sharetoQQ(String title,String summary,String target_url,String image
     bundle.putString(QQShare.SHARE_TO_QQ_SUMMARY, summary);
     bundle.putString(QQShare.SHARE_TO_QQ_TARGET_URL, target_url);
     bundle.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, image_url);
-    mTencent.shareToQQ(this.getApplicationContext(), bundle, new BaseUiListener());
+    mTencent.shareToQQ(this.cordova.getActivity(), bundle, new BaseUiListener());
   }
 
-  public void sharetoQzone(String title,String summary,String target_url,String image_url) {
+  public void sharetoQzone(String title, String summary, String target_url, String image_url) {
     Bundle bundle = new Bundle();
     bundle.clear();
     bundle.putInt(QzoneShare.SHARE_TO_QZONE_KEY_TYPE, QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT);
@@ -107,9 +86,9 @@ public void sharetoQQ(String title,String summary,String target_url,String image
     ArrayList<String> imageUrls = new ArrayList<String>();
     imageUrls.add(image_url);
     bundle.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, imageUrls);
-    mTencent.shareToQzone(this.getApplicationContext(), bundle, new BaseUiListener());
+    mTencent.shareToQzone(this.cordova.getActivity(), bundle, new BaseUiListener());
   }
-  
+
   public class BaseUiListener implements IUiListener {
 
     @Override
@@ -130,5 +109,4 @@ public void sharetoQQ(String title,String summary,String target_url,String image
 
   }
 
-  
 }
